65386620a513bc5b3ce80c25f34b6971aadd4c53
local_user <navez.martin@gmail.com>
2021-06-18 15:41:11 +0200

    3 exos done

--- src/be/digitalcity/formation/exercices/DistriBoisson.java
+++ src/be/digitalcity/formation/exercices/DistriBoisson.java
@@ -6,34 +6,31 @@ public class DistriBoisson {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         byte nbBoisonRestante = 10;
-        boolean continuer = true;
+        String reponse;
+        boolean continuer = false;
 
         do {
             System.out.printf("Quel sera ton choix dans ces boissons ? %n 1.Pepsi %n 2.fanta %n 3.Eau %n 4.Café %n 5.Thé %nEntrez le nombre de la boisson désirée :");
             String choix = scan.nextLine();
-            switch (choix){
-                case "1":
-                    System.out.println("Voici ton Pepsi");
-                    break;
-                case "2":
-                    System.out.println("Voici ton Fanta");
-                    break;
-                    
-                case "3":
-                    System.out.println("Voici ton Eau");
-                    break;
-                case "4":
-                    System.out.println("Voici ton Café");
-                    break;
-                case "5":
-                    System.out.println("Voici ton Thé");
-                    break;
-                default:
-                    System.out.println("Ce choix n'existe pas");
+            switch (choix) {
+                case "1" -> System.out.println("Voici ton Pepsi");
+                case "2" -> System.out.println("Voici ton Fanta");
+                case "3" -> System.out.println("Voici ton Eau");
+                case "4" -> System.out.println("Voici ton Café");
+                case "5" -> System.out.println("Voici ton Thé");
+                default -> System.out.println("Ce choix n'existe pas");
             }
             nbBoisonRestante --;
-            System.out.println("Veux-tu encore ");
+            System.out.println("Veux-tu encore une boisson ? (O / N)");
+            reponse = scan.nextLine();
+            if (reponse.equals("O")){
+                continuer = true;
+            }
         } while(nbBoisonRestante != 0 && continuer);
-        System.out.println("Il n'y a plus de boissons disponibles");
+        if (continuer){
+            System.out.println("Il n'y a plus de boissons disponibles");
+        } else {
+            System.out.println("Revenez quand vous voulez !");
+        }
     }
 }
